979d79c545319065df04b2a646252c0c25fa3c46
Nattapat-Piyakan <6810110098@psu.ac.th>
2025-12-19 10:58:23 +0700

    phase 3 test

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -2,6 +2,12 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { UserRole } from '../users/entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('book')
 export class BookController {
@@ -27,13 +33,18 @@ export class BookController {
     return this.bookService.update(id, updateBookDto);
   }
 
-  @Delete(':id')
+  @Delete(':id')//help me write
   remove(@Param('id') id: string) { // ลบ + ออก
-    return this.bookService.remove(id);
+    return this.bookService.remove(id);   
   }
 
-  @Patch(':id/like')
+  @Patch(':id/like')//help me write
   async likeBook(@Param('id') id: string) {
     return this.bookService.incrementLikes(id);
   }
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) { ... } //help me write
+
 }
